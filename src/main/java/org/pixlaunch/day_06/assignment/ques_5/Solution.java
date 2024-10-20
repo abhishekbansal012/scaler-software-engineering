e16b11f44905757f49212f561408de978fd09b88
Abhishek Bansal <abhishek.bansal12@gmail.com>
2024-10-20 08:13:45 +0530

    Solved Day 6 Assignments completed

--- src/main/java/org/pixlaunch/day_06/assignment/ques_5/Solution.java
+++ src/main/java/org/pixlaunch/day_06/assignment/ques_5/Solution.java
@@ -4,7 +4,22 @@ import java.util.ArrayList;
 
 public class Solution {
     public int solve(ArrayList<Integer> A) {
-        return  0;
+        return findSumOfMaxMinElement(A);
     }
 
+    private int findSumOfMaxMinElement(ArrayList<Integer> A) {
+        int min = A.get(0);
+        int max = A.get(0);
+
+        for (Integer valueOfIthElement : A) {
+            if (valueOfIthElement > max) {
+                max = valueOfIthElement;
+            }
+
+            if (valueOfIthElement < min) {
+                min = valueOfIthElement;
+            }
+        }
+        return  min+max;
+    }
 }
