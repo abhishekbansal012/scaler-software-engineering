d643930b5dfd913ee8230af6191c38ecff64b10b
Abhishek Bansal <abhishek.bansal12@gmail.com>
2024-10-24 11:21:46 +0530

    day 7 additional assignment all solved

--- src/main/java/org/pixlaunch/module_02/day_07/assignment/ques_04/Solution.java
+++ src/main/java/org/pixlaunch/module_02/day_07/assignment/ques_04/Solution.java
@@ -5,8 +5,59 @@ import java.util.ArrayList;
 public class Solution {
     public int solve(ArrayList<Integer> A) {
 
+        int count = 0;
+        for(int i =0;i<A.size();i++) {
+            if(i == 0) {
+                //Sum of Even index elements after removing ith element
+                int sumOfEvenElements = sumOfArrayForRangeOfOddIndexes(A, i+1, A.size()-1);
 
+                //Sum of Odd index elements after removing ith element
+                int sumOfOddElements = sumOfArrayForRangeOfEvenIndexes(A, i+1, A.size()-1);
 
+                if(sumOfEvenElements == sumOfOddElements) {
+                    count++;
+                }
+            } else {
+                //Sum of Even index elements after removing ith element
+                int sumOfEvenElements = sumOfArrayForRangeOfEvenIndexes(A, 0, i-1)
+                        + sumOfArrayForRangeOfOddIndexes(A, i+1, A.size()-1);
+
+                //Sum of Odd index elements after removing ith element
+                int sumOfOddElements = sumOfArrayForRangeOfOddIndexes(A, 0, i-1)
+                        + sumOfArrayForRangeOfEvenIndexes(A, i+1, A.size()-1);
+
+                if(sumOfEvenElements == sumOfOddElements) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    private int sumOfArrayForRangeOfOddIndexes(ArrayList<Integer> A, int leftIndex, int rightIndex) {
+        int sum = 0;
+
+        for(int i = leftIndex; i<=rightIndex; i++) {
+            if(i%2!=0) {
+                sum+=A.get(i);
+            }
+        }
+
+        return sum;
+
+    }
+
+    private int sumOfArrayForRangeOfEvenIndexes(ArrayList<Integer> A, int leftIndex, int rightIndex) {
+
+        int sum = 0;
+
+        for(int i = leftIndex; i<=rightIndex; i++) {
+            if(i%2==0) {
+                sum+=A.get(i);
+            }
+        }
+
+        return sum;
     }
 
 }
