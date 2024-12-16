e4efba9e53507d25376be1193d4416142999ed7a
Abhishek Bansal <abhishek.bansal12@gmail.com>
2024-12-16 09:16:30 +0530

    day 11 assignment completed

---
+++ src/main/java/org/pixlaunch/module_03/day_11/assignment/ques_01/Main.java
@@ -0,0 +1,27 @@
+package org.pixlaunch.module_03.day_11.assignment.ques_01;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Read values from user
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Enter value of Array A comma separated: ");
+        String valueOfA = myObj.next();  // Read user input
+
+
+        //Convert user String input to Array
+        ArrayList<Integer> arrayA = Arrays.stream(valueOfA.split(","))
+                .map(Integer::parseInt)
+                .collect(Collectors.toCollection(ArrayList::new));
+
+        Solution solution = new Solution();
+        System.out.println(solution.solve(arrayA));
+
+    }
+}
