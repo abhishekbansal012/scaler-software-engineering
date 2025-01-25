d1d0d91cfe004793a6e039782a00c62de937e203
Abhishek Bansal <abhishek.bansal12@gmail.com>
2025-01-25 13:58:10 +0530

    day 42 question 03 solved.

--- src/main/java/org/pixlaunch/module_05/day_42/assignment/ques_02/Solution.java
+++ src/main/java/org/pixlaunch/module_05/day_42/assignment/ques_02/Solution.java
@@ -3,8 +3,23 @@ package org.pixlaunch.module_05.day_42.assignment.ques_02;
 public class Solution {
 
     public TreeNode sortedArrayToBST(final int[] A) {
-
-        return null;
+        return constructTree(A, 0, A.length-1);
     }
+
+    private TreeNode constructTree(int[] arr, int startIndex, int endIndex) {
+
+        if(startIndex > endIndex) {
+            return null;
+        }
+
+        int middle = (startIndex+endIndex)/2;
+        TreeNode node = new TreeNode(arr[middle]);
+
+        node.left = constructTree(arr, startIndex, middle-1);
+        node.right = constructTree(arr, middle+1, endIndex);
+
+        return node;
+    }
+
 
 }
