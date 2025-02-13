bae6f16181c6e4d5d832970f7c08cf0fe77bd908
Abhishek Bansal <abhishek.bansal12@gmail.com>
2025-02-13 12:19:47 +0530

    day 38 Code. Ques-04

---
+++ src/main/java/org/pixlaunch/module_05/day_38/assignment/ques_04/Main.java
@@ -0,0 +1,16 @@
+package org.pixlaunch.module_05.day_38.assignment.ques_04;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int A=1, B=1;
+        ArrayList<Integer> C = new ArrayList<>(List.of(2));
+        Solution solution = new Solution();
+        System.out.println(solution.solve(A,B,C));
+
+    }
+}
