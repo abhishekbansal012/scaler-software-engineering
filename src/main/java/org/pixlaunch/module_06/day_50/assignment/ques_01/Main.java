0cfb6446a9f705541a3290fb78466c08dedcd86d
Abhishek Bansal <abhishek.bansal12@gmail.com>
2025-02-16 18:31:11 +0530

    day 50 Code. done

---
+++ src/main/java/org/pixlaunch/module_06/day_50/assignment/ques_01/Main.java
@@ -0,0 +1,46 @@
+package org.pixlaunch.module_06.day_50.assignment.ques_01;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ListNode A = null;
+        ListNode B = null;
+
+        // Inserting values into Linked List A
+        A = insert(A, 3);
+        A = insert(A, 4);
+        A = insert(A, 2);
+        A = insert(A, 8);
+
+        Solution solution = new Solution();
+        display(solution.sortList(A));
+
+    }
+
+    public static ListNode insert(ListNode head, int value) {
+        ListNode newNode = new ListNode(value);
+        if (head == null) {
+            return newNode;
+        }
+        ListNode temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+        return head;
+    }
+
+    public static void display(ListNode head) {
+        ListNode temp = head;
+        while (temp != null) {
+            System.out.print(temp.val);
+            if (temp.next != null) {
+                System.out.print(" -> ");
+            }
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+
+}
